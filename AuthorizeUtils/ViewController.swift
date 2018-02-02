5f0e429f1cf75dd634c3d6661df08b3bfb22229c
Dxc <daixingchuang@163.com>
2018-02-02 14:02:04 +0800

    Initial Commit

---
+++ AuthorizeUtils/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AuthorizeUtils
+//
+//  Created by Dxc_iOS on 2018/2/2.
+//  Copyright © 2018年 代星创. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
